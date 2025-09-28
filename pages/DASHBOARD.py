fba6164e889a77047253fd576e8a5b880e06d68b
Priti Dwivedi <rishu07dwivedi@gmail.com>
2025-09-28 15:47:30 +0530

    Updated Dahboard file

--- pages/DASHBOARD.py
+++ pages/DASHBOARD.py
@@ -6,9 +6,5 @@ st.title("📊 Ola Analytics Dashboard")
 
 st.write("Below is the integrated dashboard from Tableau/Power BI.")
 
-# Example Tableau Embed
-#tableau_url = "https://public.tableau.com/views/YourDashboardLinkHere"
-#st.markdown(f'<iframe src="{tableau_url}" width="100%" height="600"></iframe>', unsafe_allow_html=True)
-
-
-
+tableau_url = "https://public.tableau.com/app/profile/priti.kumari2802/viz/OLADATASETANALYSIS/Dashboard1"
+st.markdown(f'<iframe src="{tableau_url}" width="100%" height="600"></iframe>', unsafe_allow_html=True)
